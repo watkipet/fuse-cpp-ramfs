ae7301250e5acb24f17e0d5af9db1b4d0a31c849
Peter Watkins <peter@watkinselectronics.com>
2017-05-25 08:51:25 -0700

    Switching from ninja and meson for libfuse to autoconf

--- util/travis-before_install.sh
+++ util/travis-before_install.sh
@@ -2,23 +2,11 @@
 
 set -ex
 
-# pytest and meson
-sudo pip install pytest meson
-
-# ninja
-wget https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip
-unzip ninja-linux.zip
-chmod 755 ninja
-sudo chown root:root ninja
-sudo mv -fv ninja /usr/local/bin
-ninja --version
-
 # libfuse
 wget https://github.com/libfuse/libfuse/archive/fuse-3.0.2.tar.gz
 tar -xzf fuse-3.0.2.tar.gz
-mkdir build-fuse
-cd build-fuse
-meson .. 
-mesonconf -Dinstallprefix=/usr
-ninja
-sudo ninja install
+cd fuse-3.0.2
+./makeconf.sh
+./configure
+make
+sudo make install
