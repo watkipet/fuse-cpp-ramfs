801d47c68ac2fd22f432d1f4bbee977871d9bede
Peter Watkins <peter@watkinselectronics.com>
2017-05-25 08:09:58 -0700

    Added steps to get build tools for fuse first

---
+++ util/travis-before_install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -ex
+
+# pytest and meson
+sudo python3 -m pip install pytest meson
+
+# ninja
+wget https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip
+unzip ninja-linux.zip
+chmod 755 ninja
+sudo chown root:root ninja
+sudo mv -fv ninja /usr/local/bin
+ninja --version
+
+# libfuse
+wget https://github.com/libfuse/libfuse/archive/fuse-3.0.2.tar.gz
+tar -xzf fuse-3.0.2.tar.gz
+mkdir build-fuse
+cd build-fuse
+meson .. 
+mesonconf -Dinstallprefix=/usr
+ninja
+sudo ninja install
