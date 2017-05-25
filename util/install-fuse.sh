3464839ad49b8f8b12f6c80ac20198c5956b3dcb
Peter Watkins <peter@watkinselectronics.com>
2017-05-25 07:54:50 -0700

    Fixed fuse build command

--- util/install-fuse.sh
+++ util/install-fuse.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 wget https://github.com/libfuse/libfuse/archive/fuse-3.0.2.tar.gz
-tar -xzvf fuse-3.0.2.tar.gz
-md build-fuse
+tar -xzf fuse-3.0.2.tar.gz
+mkdir build-fuse
 cd build-fuse
 meson .. 
 mesonconf -Dinstallprefix=/usr
