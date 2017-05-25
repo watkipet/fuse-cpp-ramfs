b058fb74bbd75bf9926113dfdc056067d2b368c7
Peter Watkins <peter@watkinselectronics.com>
2017-05-25 08:59:45 -0700

    Travis fixes

--- util/travis-before_install.sh
+++ util/travis-before_install.sh
@@ -5,8 +5,7 @@ set -ex
 # libfuse
 wget https://github.com/libfuse/libfuse/archive/fuse-3.0.2.tar.gz
 tar -xzf fuse-3.0.2.tar.gz
-ls -al
-cd fuse-3.0.2
+cd libfuse-fuse-3.0.2
 ./makeconf.sh
 ./configure
 make
